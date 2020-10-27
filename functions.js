c36df153e33e7ef862bf5df8f376f9b0d33876f9
dianasch <dianaschao@gmail.com>
2020-10-27 22:48:14 +0000

    Completes getsFullName function.

--- functions.js
+++ functions.js
@@ -5,18 +5,26 @@
 
 // Given a `town`, return `true` if `town` is 'New York City'.
 // Arguments: 
-    // town(str): A town
+// town(str): A town
 
 // Returns:
-    // bool: true if `town` is `New York City `
+// bool: true if `town` is `New York City `
 
 const isHometown = town => town === 'New York City';
 
 
 // 2. getFullName
 
-// Define your function here
+// Given `first_name` and `last_name`, return a full name.
 
+// Arguments:
+// first_name (str): A first name
+// last_name (str): A last name
+
+// Returns:
+// str: A full name
+
+const getFullName = (firstName, lastName) => `${firstName} ${lastName}`
 
 // 3. calculateTotal
 
