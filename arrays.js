99fd1835100b172ae08447be97ab5f4c1dd83198
dianasch <dianaschao@gmail.com>
2020-10-27 22:17:45 +0000

    Completes printIndices and everyOtherItem functions.
    Co-authored-by: laura-newland <laurasmora101@gmail.com>

--- arrays.js
+++ arrays.js
@@ -3,17 +3,57 @@
 
 // 1. printIndices
 function printIndices(items) {
-  // Replace this with your code
+  // Print each item in the array, followed by its index.
+
+  // Loop through each index in the array
+  for (const i in items) {
+
+    // Print item at index i and i
+    console.log(`${items[i]} ${i}`)
+  }
+
 }
 
 
 // 2. everyOtherItem
 function everyOtherItem(items) {
-  // Replace this with your code
+  // Print an array of every other item in `items`
+
+  // Creates an empty list
+  const result = [];
+
+  // Loop through each index in the array
+  for (const i in items) {
+    
+    // Checks if i is even
+    if (i % 2 === 0) {
+
+      // If so, append the item at index i
+      result.push(items[i]);    
+    }
+
+  }
+  // prints the results
+  console.log(result)
+
 }
 
 
 // 3. smallestNItems
 function smallestNItems(items, n) {
-  // Replace this with your code
+  // Print an array of the `n` smallest integers in `items`.
+
+
+  // function compare(a, b) {
+  //   return a - b;
+  // }
+
+  items.sort((a, b) => a - b);
+
+  items.slice(0, (n - 1));
+
+  items.reverse();
+  
+  console.log(items);
+
 }
