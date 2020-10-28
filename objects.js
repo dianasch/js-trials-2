35098fcd774e1ec72aa074ab0f9a17a2c6fb0ea9
dianasch <dianaschao@gmail.com>
2020-10-28 21:34:07 +0000

    Finishes countWords function.

--- objects.js
+++ objects.js
@@ -3,7 +3,31 @@
 
 // 1. countWords
 function countWords(phrase) {
-  // Replace this with your code
+  // Return an object of each word and the no. of times it appears.
+
+  // Set variable wordCounts as an empty object
+  const wordCounts = {};
+
+  // Set variable phraseSplit as an array of the phrase split by words
+  const phraseSplit = phrase.split(' ');
+
+  // Loop through each word in phraseSplit array
+  for (const word of phraseSplit) {
+
+    // Determine if word is in wordCounts
+    if (wordCounts.hasOwnProperty(word)) {
+
+      // If so, increment value stored at word by 1
+      wordCounts[word] += 1;
+
+      // Otherwise, set value stored at word to 1
+    } else {
+      wordCounts[word] = 1;
+    }
+  }
+
+  // Return wordCounts object
+  return wordCounts;
 }
 
 
